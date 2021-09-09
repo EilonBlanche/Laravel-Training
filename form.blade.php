306f852d6956840b8f8319d2561ed37b25fe7cc5
Eilon Blanche <32070200+EilonBlanche@users.noreply.github.com>
2021-09-09 18:13:30 +0800

    Add files via upload

---
+++ form.blade.php
@@ -0,0 +1,25 @@
+<div class="container-fluid">
+    <br/>
+    <h1>Sample Form</h1>
+    <form class="form">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="form-group form-floating">
+                    <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter First Name">
+                    <label for="first_name">First Name</label>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group form-floating">
+                    <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter Last Name">
+                    <label for="last_name">Last Name</label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                <button type="submit" class="btn btn-primary form-control">Submit Form</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
